8d0b920201d75833dd4144e44d0b66cc3bff4025
mssaif-008 <mssaif081205@gmail.com>
2025-06-30 10:18:27 +0530

    initial

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { isAuthenticated } from "@/lib/actions/auth.action"
+import { redirect } from "next/navigation"
+
+const RootLayout = async ({children}: {children: ReactNode}) => {
+
+  const isUseruthenticated = await isAuthenticated();
+
+  if (!isUseruthenticated) redirect('/sign-in');
+
+  return (
+    <div className='root-layout'>
+      <nav>
+        <Link href='/' className="flex items-center gap-2">
+          <Image src='/logo.svg' alt="logo" width={38} height={32} />
+          <span>Prepper</span>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default RootLayout
